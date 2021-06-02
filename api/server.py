f24c23ba2c8b405f50099208f000c2978dcee842
Yusuke Sakai <y.f.sakai@gmail.com>
2021-06-02 08:12:36 +0000

    Fix offset of roles

--- api/server.py
+++ api/server.py
@@ -272,7 +272,8 @@ class RolesResource():
         # Get roles
         roles = RoleModel.all()
         if req_param['per_page'] > 0:
-            roles = roles.offset(req_param['per_page'] * req_param['page']).limit(req_param['per_page'])
+            offset: int = req_param['per_page'] * (req_param['page'] - 1)
+            roles = roles.offset(offset).limit(req_param['per_page'])
         if req_param['search']:
             roles = roles.filter(
                 Q(name__contains=req_param['search']) | Q(description__contains=req_param['search'])
